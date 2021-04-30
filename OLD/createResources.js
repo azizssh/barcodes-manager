2abf7672ea5d42a06cba6fecb38b8b20215255c7
Aziz Abdullaev <aliqsson@gmail.com>
2021-04-30 17:49:47 -0500

    changed

--- OLD/createResources.js
+++ OLD/createResources.js
@@ -5,3 +5,14 @@ fs.closeSync(fs.openSync(`${newFolderName}/title.txt`, "w"));
 fs.mkdirSync(`${newFolderName}/images/`, (err) => {
   if (err) console.err(err);
 });
+
+
+
+// const newFile = createLabel("Address30251", "1916111111018", 'SP-104');
+// newFile.forEach((line, idx) => console.log(idx, line))
+
+/*
+    <LabelName>Address30251</LabelName>
+              <DataString>1916111111018</DataString>
+                <Text>GC-008</Text>
+*/
--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const convertToEan = require("./convertToEan");
-require("dotenv").config();
-const {PRODUCT_PATH, QUERY_PRODCODE, QUERY_DESCRIPTION} = require('./utils/constants');
+const {
+  PRODUCT_PATH,
+  QUERY_PRODCODE,
+  QUERY_DESCRIPTION,
+  EXCEL_FILENAME,
+} = require("./utils/constants");
+const path = require('path')
 const readline = require("readline");
 const createProductFolder = require("./utils/createProductFolder");
 const addToExcel = require("./addToExcel");
 const getValidProductCode = require("./utils/getValidProductCode");
-const log = console.log;
+const dataToHashMap = require("./utils/dataToHashMap");
+const loadExcel = require("./utils/loadExcel");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const productsHash = dataToHashMap(
+  loadExcel(path.resolve(PRODUCT_PATH, EXCEL_FILENAME))
+);
+
 const recursiveAsyncReadLine = function () {
   rl.question(QUERY_PRODCODE, function (productCode) {
-    const validProductCode = getValidProductCode(productCode)
+    const validProductCode = getValidProductCode(productCode);
     if (productCode == "exit")
       //we need some base case, for recursion
       return rl.close(); //closing RL and returning from function.
     const ean = convertToEan(validProductCode);
-    log("EAN-13:", ean);
     //Calling this function again to ask new question
     rl.question(QUERY_DESCRIPTION, (desc) => {
       createProductFolder(PRODUCT_PATH, ean, validProductCode);
-      addToExcel([validProductCode, desc, ean]);
+      if (!productsHash.hasOwnProperty(validProductCode)) {
+        addToExcel([validProductCode, desc, ean]);
+      } else {
+        console.log('The product is already present in the spreadsheet');
+      }
       recursiveAsyncReadLine();
     });
   });
